acc186a37c5e7ae0a6eebc7c08d0a6749c104d9a
Riley Mathews <rileymathews80@gmail.com>
2019-08-07 21:48:26 -0500

    made 0.0.0.0 default server port

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@
 import os
 import sys
 
+from django.core.management.commands.runserver import Command as runserver
+runserver.default_addr = "0.0.0.0"
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
