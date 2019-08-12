b5fca6138d2be92aec07bc72350e871ba728b86f
Riley Mathews <rileymathews80@gmail.com>
2019-08-11 23:06:42 -0500

    removed uneeded config

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: "babel-loader"
       }
     ]
   },
