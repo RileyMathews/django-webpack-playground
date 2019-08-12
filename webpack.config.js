345b7a9ee18036148596c1d587c7d9cd9cf0cef9
Riley Mathews <rileymathews80@gmail.com>
2019-08-11 20:28:55 -0500

    added babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ const path = require('path');
 
 module.exports = {
   entry: './assets/index.js',
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'static')
